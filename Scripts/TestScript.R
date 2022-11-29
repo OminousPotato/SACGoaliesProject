18cd74ff21f03504f408e921e31eac101de5cb72
Brian Yngve <67799535+OminousPotato@users.noreply.github.com>
2022-11-28 19:29:39 -0500

    Add GSAA Chart

--- Scripts/TestScript.R
+++ Scripts/TestScript.R
@@ -3,32 +3,55 @@ library(ggplot2)
 
 data <- read.csv("./Data/hockeyReference.csv")
 
+colors <- read.csv("./Data/colors.csv")
 
 clean_data <- data %>% 
   filter(GP >= 20) %>% 
-  select(Player, Tm, GP, SV., SO)
+  select(Player, Tm, GP, SV., SO, GSAA)
 
+complete_data <- merge(clean_data, colors, by="Tm")
 
 avgSV = mean(clean_data$SV.)
 
 avgSO = mean(clean_data$SO)
 
-upper_quadrant <- clean_data %>% 
+upper_quadrant <- complete_data %>% 
   filter(SV. > avgSV & SO > avgSO)
 
-
-ggplot(clean_data, aes(x=SV., y=SO)) + 
-  geom_point() + 
-  xlim(.875, .94) +
-  ylim(0, 10) + 
-  geom_hline(yintercept = avgSO) +
-  geom_vline(xintercept = avgSV) + 
+ggplot(upper_quadrant, aes(x = reorder(Player, GSAA), y = GSAA, fill=Color)) +
+  geom_bar(stat="identity") +
+  scale_fill_identity() +
   labs(
-    title = "Standout NHL Goalies from 2021-22 Season",
-    x = "Save Percentage",
-    y = "Shutouts Recorded",
-    caption = "Data from hockey-reference\nData Viz by Brian^2 and Matt Roepke"
+    title = "Goalies with Most Goals Saved Above Average",
+    x = "",
+    y = "Goals Saved Above Average",
+    caption = "Data from hockey-reference\nData Viz by Brian^2 and Matt Roepke",
+    subtitle = "Colored by Team"
   ) +
-  
-  theme(plot.title = element_text(hjust = .5))
+  theme_bw() +
+  theme(
+    axis.text.x=element_text(angle=45, hjust=1),
+    plot.title = element_text(hjust = .5),
+    plot.subtitle = element_text(hjust = .5)
+  )
 
+ggsave("Charts/GSAA_v2.png")
+
+# ggplot(complete_data, aes(x=SV., y=SO, color=Color)) +
+#   geom_point() +
+#   xlim(.875, .94) +
+#   ylim(0, 10) +
+#   geom_hline(yintercept = avgSO) +
+#   geom_vline(xintercept = avgSV) +
+#   scale_color_identity() +
+#   labs(
+#     title = "Standout NHL Goalies from 2021-22 Season",
+#     x = "Save Percentage",
+#     y = "Shutouts Recorded",
+#     caption = "Data from hockey-reference\nData Viz by Brian^2 and Matt Roepke"
+#   ) +
+#   theme_bw() +
+#   theme(plot.title = element_text(hjust = .5))
+
+
+ggsave("Charts/Version2.png")
