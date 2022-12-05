0d4a869808f0dc93dbfdd5ee3f7b57c380580333
Brian Yngve <67799535+OminousPotato@users.noreply.github.com>
2022-12-05 14:37:00 -0500

    Add colors to dot chart

--- Scripts/TestScript.R
+++ Scripts/TestScript.R
@@ -5,9 +5,17 @@ data <- read.csv("./Data/hockeyReference.csv")
 
 colors <- read.csv("./Data/colors.csv")
 
+teamData <- read.csv("Data/teamData.csv")
+
 clean_data <- data %>% 
   filter(GP >= 20) %>% 
   select(Player, Tm, GP, SV., SO, GSAA)
+
+clean_teamData <- teamData %>% 
+  mutate(Diff = GF - GA) %>% 
+  select(Tm, Diff, GF, GA)
+
+clean_data <- merge(clean_data, clean_teamData, by="Tm")
 
 complete_data <- merge(clean_data, colors, by="Tm")
 
@@ -18,24 +26,32 @@ avgSO = mean(clean_data$SO)
 upper_quadrant <- complete_data %>% 
   filter(SV. > avgSV & SO > avgSO)
 
-ggplot(upper_quadrant, aes(x = reorder(Player, GSAA), y = GSAA, fill=Color)) +
-  geom_bar(stat="identity") +
-  scale_fill_identity() +
-  labs(
-    title = "Goalies with Most Goals Saved Above Average",
-    x = "",
-    y = "Goals Saved Above Average",
-    caption = "Data from hockey-reference\nData Viz by Brian^2 and Matt Roepke",
-    subtitle = "Colored by Team"
-  ) +
-  theme_bw() +
-  theme(
-    axis.text.x=element_text(angle=45, hjust=1),
-    plot.title = element_text(hjust = .5),
-    plot.subtitle = element_text(hjust = .5)
-  )
+############################################################
+## GSAA Plot
+############################################################
 
-ggsave("Charts/GSAA_v2.png")
+# ggplot(upper_quadrant, aes(x = reorder(Player, GSAA), y = GSAA, fill=Color)) +
+#   geom_bar(stat="identity") +
+#   scale_fill_identity() +
+#   labs(
+#     title = "Goalies with Most Goals Saved Above Average",
+#     x = "",
+#     y = "Goals Saved Above Average",
+#     caption = "Data from hockey-reference\nData Viz by Brian^2 and Matt Roepke",
+#     subtitle = "Colored by Team"
+#   ) +
+#   theme_bw() +
+#   theme(
+#     axis.text.x=element_text(angle=45, hjust=1),
+#     plot.title = element_text(hjust = .5),
+#     plot.subtitle = element_text(hjust = .5)
+#   )
+# 
+# ggsave("Charts/GSAA_v2.png")
+
+############################################################
+## Shutouts and Save % Plot
+############################################################
 
 # ggplot(complete_data, aes(x=SV., y=SO, color=Color)) +
 #   geom_point() +
@@ -52,6 +68,7 @@ ggsave("Charts/GSAA_v2.png")
 #   ) +
 #   theme_bw() +
 #   theme(plot.title = element_text(hjust = .5))
+# 
+# 
+# ggsave("Charts/Version2.png")
 
-
-ggsave("Charts/Version2.png")
